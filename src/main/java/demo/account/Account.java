5ae68caab6e0afed5f3952891fec75a1da7faf26
laphuocnguyen <phuocnguyen344@gmail.com>
2021-10-15 22:02:13 +0700

    add String value

--- src/main/java/demo/account/Account.java
+++ src/main/java/demo/account/Account.java
@@ -24,7 +24,7 @@ public class Account extends BaseEntity {
     private Boolean defaultAccount;
     private Set<CreditCard> creditCards;
     private Set<Address> addresses;
-
+    private String test;
     public Account() {
     }
 
